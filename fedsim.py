328bc80977db0ebefbe8da23ce9b72c1e2acf5a0
Luke Palmer <lrpalmer@gmail.com>
2021-08-07 12:27:36 -0400

    New trade model

--- fedsim.py
+++ fedsim.py
@@ -10,126 +10,116 @@ import time
 Good = namedtuple('Good', ['name'])
 QGood = namedtuple('QGood', ['good', 'qty'])
 
-Ask = recordclass('Ask', ['price', 'qty', 'asker'])
-Bid = recordclass('Ask', ['price', 'qty', 'bidder'])
+TRADE = 0
+TRADEQTY = 0
 
-class Market:
-    def __init__(self, good, initialPrice, maxInventory):
-        self.good = good
-        self.maxInventory = maxInventory
-        self.alpha = initialPrice / maxInventory
+def seqClosure(a, b):
+    def r():
+        a()
+        b()
+    return r
+def cantBuyNothing():
+    assert False
 
-        self.askVolume = 0
-        self.bidVolume = 0
+def marketBuy(markets, good, requested):
+    buyClosure = lambda: None
+    totalPrice = 0
+    totalQty = 0
+    while requested > 0:
+        bestPrice = float('inf')
+        bestQty = 0
+        bestClosure = None
+        bestMarket = None
+        for m in markets:
+            price, qty, closure = m.askPrice(good, requested)
+            if qty == 0:
+                continue
+            pricePer = price / qty
+            if pricePer < bestPrice:
+                bestPrice = pricePer
+                bestQty = qty
+                bestClosure = closure
+                bestMarket = m
+        if bestMarket is None:
+            break
+        else:
+            totalPrice += bestPrice * bestQty
+            totalQty += bestQty
+            requested -= bestQty
+            buyClosure = seqClosure(buyClosure, bestClosure)
+            
+    return (totalPrice, totalQty, buyClosure)
 
-        self.drift = 1 + math.exp(random.uniform(math.log(0.001), math.log(0.01)))
-
-        self.inventory = 0
-
-    def ask(self, qty):
-        total = 0
-
-        for i in range(qty):
-            total += self.price()
-            self.inventory += 1
-            self.askVolume += 1
-
-        return total
-
-    def bid(self, qty, cap=float('inf')):
-        total = 0
-        bought = 0
-
-        while bought < qty and self.inventory > 0 and self.price() <= cap:
-            price = self.price()
-            total += price
-            cap -= price
-            bought += 1
-            self.inventory -= 1
-            self.bidVolume += 1
-
-        return (bought, total)
-
-    def price(self):
-        return self.alpha * max(0, self.maxInventory - self.inventory)
-
-    def step(self):
-        if self.inventory == 0:
-            p = 1 - 2 * self.inventory / self.maxInventory
-            self.alpha *= math.pow(self.drift, p)
-
-    def report(self):
-        volume = min(self.askVolume, self.bidVolume)
-        self.askVolume -= volume
-        self.bidVolume -= volume
-        print(f'Market for {self.good}: ${self.price():.2f} - VOL {volume} - INV {self.inventory}')
-
-        return volume
+def marketBuyMany(markets, inputs):
+    buyClosure = lambda: None
+    totalPrice = 0
+    for i in inputs:
+        price, qty, closure = marketBuy(markets, i.good, i.qty)
+        assert qty <= i.qty
+        if qty < i.qty:
+            return (float('inf'), cantBuyNothing)
+        totalPrice += price
+        buyClosure = seqClosure(buyClosure, closure)
+    return (totalPrice, buyClosure)
 
 class Producer:
-    def __init__(self, output, goodsRequired, scale):
-        self.good = output.good
-        self.quantityProduced = output.qty
-        self.goodsRequired = goodsRequired
-        self.scale = scale
-        self.inventory = { req.good: 0 for req in goodsRequired }
-        self.idleSteps = 0
-        self.capital = 0
-        self.alive = True
+    def __init__(self, output, inputs, capital, initialPrice = float('inf'), initialInventory = 0):
+        self._output = output
+        self._inputs = inputs
+        self._price = initialPrice
+        self._inventory = initialInventory
+        self._maxInventory = 10 * self._output.qty
+        self._capital = capital
 
-    def operate(self):
-        self.idleSteps += 1
+    def goodsProduced(self):
+        if self._inventory > 0:
+            return set([self._output.good])
+        else:
+            return set()
 
-        self.inventory.setdefault(self.good, 0)
-        if self.inventory[self.good] < self.scale * self.quantityProduced:
-            scale = self.scale
-            while scale > 0:
-                scale -= 1
-                for req in self.goodsRequired:
-                    dq = req.qty - self.inventory[req.good]
-                    if dq > 0:
-                        received, cost = getMarket(req.good).bid(dq)
-                        self.capital -= cost
-                        self.inventory[req.good] += received
+    def askPrice(self, good, qty):
+        def buyClosure(qty):
+            def r():
+                assert self._inventory >= qty
+                self._inventory -= qty
+                self._capital += self._price * qty
+                global TRADE
+                global TRADEQTY
+                TRADE += self._price * qty
+                TRADEQTY += qty
+            return r
 
-                    if self.inventory[req.good] < req.qty:
-                        scale = 0
-                        break
-                else:
-                    for req in self.goodsRequired:
-                        self.inventory[req.good] -= req.qty
-                    self.inventory[self.good] += self.quantityProduced
+        if good != self._output.good:
+            return (float('inf'), 0, cantBuyNothing)
+        if self._inventory >= qty:
+            return (qty * self._price, qty, buyClosure(qty))
+        else:
+            return (self._inventory * self._price, self._inventory, buyClosure(self._inventory))
 
-        market = getMarket(self.good)
-        while self.inventory[self.good] * market.price() > max(0, -self.capital) and self.inventory[self.good] > 0:
-            self.capital += market.ask(1)
-            self.inventory[self.good] -= 1
-            self.idleSteps = 0
+    def operate(self, markets):
+        if self._inventory >= self._maxInventory:
+            return
+
+        price, buyClosure = marketBuyMany(markets, self._inputs)
+        if price < self._capital:
+            self._capital -= price
+            buyClosure()
+            self._inventory += self._output.qty
+            self._price = price
 
     def report(self):
-        reportLine = f'{self.quantityProduced} {self.good} <- '
-        for req in self.goodsRequired:
+        reportLine = f'{self._output.qty} {self._output.good} @ ${self._price:.2f} <- '
+        for req in self._inputs:
             reportLine += f'{req.qty} {req.good}, '
         print(reportLine)
-        print(f'    ${self.capital:0.2f} | {self.inventory}')
+        print(f'    ${self._capital:0.2f} | {self._inventory}')
 
-
-MARKETS = {}
-
-def getMarket(good):
-    m = MARKETS.get(good)
-    if m is None:
-        m = MARKETS[good] = Market(good, 1, 1000)
-    return m
 
 PRODUCERS = [
-    Producer(QGood(Good('water'), 10), [ QGood(Good('labor'), 1) ], 1),
-    Producer(QGood(Good('labor'), 1), [ QGood(Good('corn'), 1), QGood(Good('water'), 1) ], 1),
-    Producer(QGood(Good('corn'), 10), [ QGood(Good('labor'), 1), QGood(Good('water'), 1) ], 1),
+    Producer(QGood(Good('water'), 10), [ QGood(Good('labor'), 1) ], 1000, initialInventory=100, initialPrice=1),
+    Producer(QGood(Good('labor'), 1), [ QGood(Good('corn'), 1), QGood(Good('water'), 1) ], 1000, initialInventory=100, initialPrice=1),
+    Producer(QGood(Good('corn'), 10), [ QGood(Good('labor'), 1), QGood(Good('water'), 1) ], 1000),
 ]
-
-getMarket(Good('labor')).ask(100)
-getMarket(Good('water')).ask(100)
 
 GOODS = [ Good(s) for s in [ 'labor', 'water', 'corn', 'wheat', 'apples', 'wood', 'ore', 'house', 'meat', 'spear' ] ]
 
@@ -137,7 +127,7 @@ def make_producer():
     inputGoods = []
     inputGoods.append(QGood(Good('labor'), random.randrange(1,4)))
 
-    goodsProduced = [ g for g in GOODS if g in MARKETS ]
+    goodsProduced = list(set.union(*[ p.goodsProduced() for p in PRODUCERS ]))
 
     for i in range(random.randrange(0,4)):
         good = random.choice(goodsProduced)
@@ -146,28 +136,25 @@ def make_producer():
         inputGoods.append(QGood(good, random.randrange(1,4)))
 
     multiplier = random.randrange(1,10)
-    return Producer(QGood(random.choice(GOODS), multiplier), inputGoods, 1)
+    return Producer(QGood(random.choice(GOODS), multiplier), inputGoods, 1000)
 
 ADD_PRODUCER = False
 
 def add_producer(sig, frame):
     global ADD_PRODUCER
+    if ADD_PRODUCER:
+        sys.exit(0)
     ADD_PRODUCER = True
 signal.signal(signal.SIGINT, add_producer)
 
 STEP = 0
 while True:
     for p in sorted(PRODUCERS, key=lambda _: random.uniform(0,1)):
-        p.operate()
-
-    for m in MARKETS.values():
-        m.step()
+        p.operate(PRODUCERS)
 
     if ADD_PRODUCER:
         ADD_PRODUCER = False
         PRODUCERS.append(make_producer())
-    if STEP % 50 == 0:
-        PRODUCERS = list(filter(lambda p: p.alive, PRODUCERS))
 
     if STEP % 1 == 0:
         print()
@@ -175,11 +162,10 @@ while True:
         for p in PRODUCERS:
             p.report()
 
-        gdp = 0
-        for g in sorted(MARKETS.keys()):
-            gdp += MARKETS[g].report()
-
-        print(f'GDP: {gdp}')
-        time.sleep(0.05)
+        print(f'GDP: {TRADEQTY} @ ${TRADE:.2f}')
+        TRADE = 0
+        TRADEQTY = 0
+        
+        time.sleep(0.4)
 
     STEP += 1
